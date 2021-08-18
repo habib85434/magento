2cb69ab2edaaa8953b87ef9b085766e45858f220
habib85434 <hrekns@gmail.com>
2021-08-18 16:35:57 +0600

    Console command created

---
+++ app/code/Workstation/SampleModule/Console/Command/AddItem.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace Workstation\SampleModule\Console\Command;
+
+use Magento\Framework\Console\Cli;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use \Workstation\SampleModule\Model\ItemFactory;
+
+class AddItem extends Command
+{
+    const INPUT_KEY_NAME = 'name';
+    const INPUT_KEY_DESCRIPTION = 'description';
+
+    private $itemFactory;
+
+    public function __construct(ItemFactory $itemFactory)
+    {
+        $this->itemFactory = $itemFactory;
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setName('workstation:item:add')
+            ->addArgument(
+                self::INPUT_KEY_NAME,
+                InputArgument::REQUIRED,
+                'Item name'
+            )->addArgument(
+                self::INPUT_KEY_DESCRIPTION,
+                InputArgument::OPTIONAL,
+                'Item description'
+            );
+        parent::configure();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $item = $this->itemFactory->create();
+        $item->setName($input->getArgument(self::INPUT_KEY_NAME));
+        $item->setDescription($input->getArgument(self::INPUT_KEY_DESCRIPTION));
+        $item->setIsObjectNew(true);
+        $item->save();
+        return Cli::RETURN_SUCCESS;
+    }
+}
